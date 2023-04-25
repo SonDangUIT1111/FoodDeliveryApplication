88cfcc22dd937f55bea00c03e5f60c1f46546042
trần khải <21522185@gm.uit.edu.vn>
2023-04-25 16:53:13 +0700

    First up - login and sign up screen

---
+++ app/src/main/java/com/example/fooddeliveryapplication/LoginActivity.java
@@ -0,0 +1,27 @@
+package com.example.fooddeliveryapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
+        TextView signup = findViewById(R.id.donthaveaccText);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this,SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
