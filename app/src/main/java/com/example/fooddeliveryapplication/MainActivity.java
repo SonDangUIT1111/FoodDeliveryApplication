ca8aec0067943b9d1113d3aca9b1c1ba32eb200b
= <=>
2023-04-25 15:27:43 +0700

    Navigation to ProfileFragment from MainActivity

--- app/src/main/java/com/example/fooddeliveryapplication/MainActivity.java
+++ app/src/main/java/com/example/fooddeliveryapplication/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.fooddeliveryapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.fooddeliveryapplication.Fragments.ProfileFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNavigationView;
+    private Fragment selectorFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        // Navigation item selected
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.nav_home:
+                        // TODO - Open HomeFragment
+                        break;
+                    case R.id.nav_favorite:
+                        // TODO - Open FavoriteFragment
+                        break;
+                    case R.id.nav_profile:
+                        selectorFragment = new ProfileFragment();
+                        break;
+                    case R.id.nav_history:
+                        // TODO - Open HistoryFragment
+                        break;
+                }
+
+                if (selectorFragment != null) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectorFragment).commit();
+                }
+
+                return true;
+            }
+        });
     }
 }
