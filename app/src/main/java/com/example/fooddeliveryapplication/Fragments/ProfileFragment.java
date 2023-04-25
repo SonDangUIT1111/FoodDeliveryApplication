f1a26741a1b3eb0a302485ca9567fe6d55eb7911
= <=>
2023-04-25 22:49:11 +0700

    Front end for ProfileFragment.

--- app/src/main/java/com/example/fooddeliveryapplication/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/fooddeliveryapplication/Fragments/ProfileFragment.java
@@ -1,20 +1,21 @@
 package com.example.fooddeliveryapplication.Fragments;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.example.fooddeliveryapplication.R;
 
 public class ProfileFragment extends Fragment {
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        return view;
     }
 }
