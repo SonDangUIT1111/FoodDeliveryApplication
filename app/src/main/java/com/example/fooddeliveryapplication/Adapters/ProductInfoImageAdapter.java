8f6cacf6d6c4f0b9a800be85365fce14b7800fd2
Dang Thai Son <sondanguit@gmail.com>
2023-04-28 10:52:39 +0700

    ImagePagerForProductInfo

---
+++ app/src/main/java/com/example/fooddeliveryapplication/Adapters/ProductInfoImageAdapter.java
@@ -0,0 +1,56 @@
+package com.example.fooddeliveryapplication.Adapters;
+
+import android.content.Context;
+import android.net.Uri;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.bumptech.glide.Glide;
+
+public class ProductInfoImageAdapter extends PagerAdapter {
+
+    Context mContext;
+
+    public ProductInfoImageAdapter(Context context) {
+        this.mContext = context;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == ((ImageView) object);
+    }
+
+    private String[] sliderImageURL = new String[]{};
+
+    public  void insertImageUrl(String[] values)
+    {
+        sliderImageURL = values;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        ImageView imageView = new ImageView(mContext);
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        Glide.with(mContext)
+                .asBitmap()
+                .load(sliderImageURL[position])
+                .into(imageView);
+        ((ViewPager) container).addView(imageView, 0);
+        return imageView;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        ((ViewPager) container).removeView((ImageView) object);
+    }
+
+    @Override
+    public int getCount() {
+        return sliderImageURL.length;
+    }
+}
