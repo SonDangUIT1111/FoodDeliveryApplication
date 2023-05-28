1a95bfbcd86b03f66b97561718685ca22f81a39d
Dang Thai Son <sondanguit@gmail.com>
2023-05-28 16:31:04 +0700

    Complete_Favourite_Food_Fragment

--- app/src/main/java/com/example/fooddeliveryapplication/Fragments/Home/FavoriteFragment.java
+++ app/src/main/java/com/example/fooddeliveryapplication/Fragments/Home/FavoriteFragment.java
@@ -4,18 +4,30 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
+import com.example.fooddeliveryapplication.Adapters.HomeAdapter.FavouriteFoodAdapter;
+import com.example.fooddeliveryapplication.Adapters.HomeAdapter.FindAdapter;
+import com.example.fooddeliveryapplication.Helpers.FirebaseFavouriteUserHelper;
+import com.example.fooddeliveryapplication.Model.Product;
+import com.example.fooddeliveryapplication.R;
 import com.example.fooddeliveryapplication.databinding.FragmentFavoriteBinding;
 
+import java.util.ArrayList;
 
 
 public class FavoriteFragment extends Fragment {
-    FragmentFavoriteBinding binding;
-    private  String userId;
+    RecyclerView recFavouriteFood;
+    View view;
+    String userId;
+    ProgressBar progressBarFavouriteList;
     public FavoriteFragment(String id ) {
         userId = id;
     }
@@ -23,8 +35,44 @@ public class FavoriteFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        binding=FragmentFavoriteBinding.inflate(inflater,container,false);
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_favorite,container,false);
 
-        return binding.getRoot();
+        // find view by id
+        recFavouriteFood = (RecyclerView) view.findViewById(R.id.recFavouriteFood);
+        progressBarFavouriteList = (ProgressBar) view.findViewById(R.id.progressBarFavouriteList);
+
+        readFavouriteList();
+        return view;
+    }
+
+    public void readFavouriteList()
+    {
+        new FirebaseFavouriteUserHelper().readFavouriteList(userId, new FirebaseFavouriteUserHelper.DataStatus() {
+            @Override
+            public void DataIsLoaded(ArrayList<Product> favouriteProducts,ArrayList<String> keys) {
+                StaggeredGridLayoutManager layoutManager=new StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL);
+                recFavouriteFood.setLayoutManager(layoutManager);
+                recFavouriteFood.setHasFixedSize(true);
+                FavouriteFoodAdapter adapter = new FavouriteFoodAdapter(getContext(),favouriteProducts,userId);
+                recFavouriteFood.setAdapter(adapter);
+                progressBarFavouriteList.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void DataIsInserted() {
+
+            }
+
+            @Override
+            public void DataIsUpdated() {
+
+            }
+
+            @Override
+            public void DataIsDeleted() {
+
+            }
+        });
     }
 }
