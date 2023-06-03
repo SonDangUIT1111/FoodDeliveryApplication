ea6cfed3ab9df8206447fbd8ec3721201220ee43
trần khải <21522185@gm.uit.edu.vn>
2023-06-03 23:40:21 +0700

    issue

--- app/src/main/java/com/example/fooddeliveryapplication/GlobalConfig.java
+++
@@ -1,6 +0,0 @@
-package com.example.fooddeliveryapplication;
-
-public class GlobalConfig {
-    public static String choseAddressId;
-    public static String updateAddressId;
-}
--- app/src/main/java/com/example/fooddeliveryapplication/Interfaces/IAddressAdapterListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.fooddeliveryapplication.Interfaces;
-
-import com.example.fooddeliveryapplication.Model.Address;
-
-public interface IAddressAdapterListener {
-    void onCheckedChanged(Address selectedAddress);
-}
