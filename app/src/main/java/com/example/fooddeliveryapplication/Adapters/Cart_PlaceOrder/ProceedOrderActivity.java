0dbf2a1f0fa15922dd4b080ba3d5158e16664363
Đỗ Minh Nhật <dmnhat.pt@gmail.com>
2023-06-10 11:42:46 +0700

    Update Bill using filter map.

--- app/src/main/java/com/example/fooddeliveryapplication/Adapters/Cart_PlaceOrder/ProceedOrderActivity.java
+++ app/src/main/java/com/example/fooddeliveryapplication/Adapters/Cart_PlaceOrder/ProceedOrderActivity.java
@@ -27,6 +27,7 @@ import com.example.fooddeliveryapplication.Model.Product;
 import com.example.fooddeliveryapplication.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
@@ -37,6 +38,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Set;
 
 public class ProceedOrderActivity extends AppCompatActivity {
     private TextView receiverName;
@@ -80,29 +82,63 @@ public class ProceedOrderActivity extends AppCompatActivity {
         complete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // Filter new map to add to bills
+                HashMap<String, CartInfo> cartInfoMap = new HashMap<>();
                 for (CartInfo cartInfo : cartInfoList) {
-                    // Add bills
-                    FirebaseDatabase.getInstance().getReference().child("Products").child(cartInfo.getProductId()).addListenerForSingleValueEvent(new ValueEventListener() {
-                        @Override
-                        public void onDataChange(@NonNull DataSnapshot snapshot) {
-                            Product product = snapshot.getValue(Product.class);
-                            FirebaseDatabase.getInstance().getReference().child("Bills").addListenerForSingleValueEvent(new ValueEventListener() {
+                    cartInfoMap.put(cartInfo.getProductId(), cartInfo);
+                }
+
+                Set<String> cartInfoKeySet = cartInfoMap.keySet();
+                HashMap<String, List<CartInfo>> filterCartInfoMap = new HashMap<>();
+                FirebaseDatabase.getInstance().getReference().child("Products").addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        for (DataSnapshot ds : snapshot.getChildren()) {
+                            Product product = ds.getValue(Product.class);
+                            for (String productId : cartInfoKeySet) {
+                                if (product.getProductId().equals(productId)) {
+                                    if (filterCartInfoMap.containsKey(product.getPublisherId())) {
+                                        filterCartInfoMap.get(product.getPublisherId()).add(cartInfoMap.get(productId));
+                                    }
+                                    else {
+                                        List<CartInfo> tempList = new ArrayList<>();
+                                        tempList.add(cartInfoMap.get(productId));
+                                        filterCartInfoMap.put(product.getPublisherId(), tempList);
+                                    }
+                                }
+                            }
+                        }
+
+                        // Loop through all cartInfo
+                        Set<String> filterCartInfoKeySet = filterCartInfoMap.keySet();
+                        for (String senderId : filterCartInfoKeySet) {
+                            // Add to Bills
+                            String billId = FirebaseDatabase.getInstance().getReference().push().getKey();
+                            SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+                            Date date = new Date();
+
+                            HashMap<String, Object> map = new HashMap<>();
+                            map.put("addressId", GlobalConfig.choseAddressId);
+                            map.put("billId", billId);
+                            map.put("orderDate", formatter.format(date));
+                            map.put("orderStatus", "Confirm");
+                            map.put("recipientId", userId);
+                            map.put("senderId", senderId);
+                            map.put("checkAllComment", false);
+                            map.put("totalPrice", convertMoneyToValue(totalPriceDisplay));
+                            FirebaseDatabase.getInstance().getReference().child("Bills").child(billId).setValue(map).addOnCompleteListener(new OnCompleteListener<Void>() {
                                 @Override
-                                public void onDataChange(@NonNull DataSnapshot snapshot1) {
-                                    boolean isHavingBill = false;
-                                    for (DataSnapshot ds : snapshot1.getChildren()) {
-                                        Bill bill = ds.getValue(Bill.class);
-
-                                        if (bill.getSenderId().equals(product.getPublisherId())) {
-                                            isHavingBill = true;
-
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        List<CartInfo> cartInfoList1 = filterCartInfoMap.get(senderId);
+                                        for (CartInfo cartInfo : cartInfoList1) {
                                             // Add to BillInfos
                                             String billInfoId = FirebaseDatabase.getInstance().getReference().push().getKey();
                                             HashMap<String, Object> map1 = new HashMap<>();
                                             map1.put("amount", cartInfo.getAmount());
                                             map1.put("billInfoId", billInfoId);
                                             map1.put("productId", cartInfo.getProductId());
-                                            FirebaseDatabase.getInstance().getReference().child("BillInfos").child(bill.getBillId()).child(billInfoId).setValue(map1);
+                                            FirebaseDatabase.getInstance().getReference().child("BillInfos").child(billId).child(billInfoId).setValue(map1);
 
                                             // Update Carts and CartInfos
                                             FirebaseDatabase.getInstance().getReference().child("Carts").addListenerForSingleValueEvent(new ValueEventListener() {
@@ -136,98 +172,25 @@ public class ProceedOrderActivity extends AppCompatActivity {
 
                                                 }
                                             });
-
-                                            Toast.makeText(ProceedOrderActivity.this, "Order completed!", Toast.LENGTH_SHORT).show();
-                                            break;
                                         }
                                     }
-
-                                    if (!isHavingBill) {
-                                        // Add to Bills
-                                        String billId = FirebaseDatabase.getInstance().getReference().push().getKey();
-                                        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
-                                        Date date = new Date();
-                                        Product product = snapshot.getValue(Product.class);
-
-                                        HashMap<String, Object> map = new HashMap<>();
-                                        map.put("addressId", GlobalConfig.choseAddressId);
-                                        map.put("billId", billId);
-                                        map.put("orderDate", formatter.format(date));
-                                        map.put("orderStatus", "Confirm");
-                                        map.put("recipientId", userId);
-                                        map.put("senderId", product.getPublisherId());
-                                        map.put("checkAllComment", false);
-                                        map.put("totalPrice", convertMoneyToValue(totalPriceDisplay));
-                                        FirebaseDatabase.getInstance().getReference().child("Bills").child(billId).setValue(map).addOnCompleteListener(new OnCompleteListener<Void>() {
-                                            @Override
-                                            public void onComplete(@NonNull Task<Void> task) {
-                                                if (task.isSuccessful()) {
-                                                    // Add to BillInfos
-                                                    String billInfoId = FirebaseDatabase.getInstance().getReference().push().getKey();
-                                                    HashMap<String, Object> map1 = new HashMap<>();
-                                                    map1.put("amount", cartInfo.getAmount());
-                                                    map1.put("billInfoId", billInfoId);
-                                                    map1.put("productId", cartInfo.getProductId());
-                                                    FirebaseDatabase.getInstance().getReference().child("BillInfos").child(billId).child(billInfoId).setValue(map1);
-
-                                                    // Update Carts and CartInfos
-                                                    FirebaseDatabase.getInstance().getReference().child("Carts").addListenerForSingleValueEvent(new ValueEventListener() {
-                                                        @Override
-                                                        public void onDataChange(@NonNull DataSnapshot snapshot) {
-                                                            for (DataSnapshot ds : snapshot.getChildren()) {
-                                                                Cart cart = ds.getValue(Cart.class);
-                                                                if (cart.getUserId().equals(userId)) {
-                                                                    FirebaseDatabase.getInstance().getReference().child("CartInfos").child(cart.getCartId()).child(cartInfo.getCartInfoId()).removeValue();
-                                                                    FirebaseDatabase.getInstance().getReference().child("Products").child(cartInfo.getProductId()).addListenerForSingleValueEvent(new ValueEventListener() {
-                                                                        @Override
-                                                                        public void onDataChange(@NonNull DataSnapshot snapshot) {
-                                                                            Product product = snapshot.getValue(Product.class);
-                                                                            FirebaseDatabase.getInstance().getReference().child("Carts").child(cart.getCartId()).child("totalAmount").setValue(cart.getTotalAmount() - cartInfo.getAmount());
-                                                                            FirebaseDatabase.getInstance().getReference().child("Carts").child(cart.getCartId()).child("totalPrice").setValue(cart.getTotalPrice() - cartInfo.getAmount() * product.getProductPrice());
-                                                                        }
-
-                                                                        @Override
-                                                                        public void onCancelled(@NonNull DatabaseError error) {
-
-                                                                        }
-                                                                    });
-
-                                                                    FirebaseDatabase.getInstance().getReference().child("CartInfos").child(cart.getCartId()).child(cartInfo.getCartInfoId()).removeValue();
-                                                                }
-                                                            }
-                                                        }
-
-                                                        @Override
-                                                        public void onCancelled(@NonNull DatabaseError error) {
-
-                                                        }
-                                                    });
-
-                                                    Toast.makeText(ProceedOrderActivity.this, "Order completed!", Toast.LENGTH_SHORT).show();
-                                                }
-                                            }
-                                        });
-                                    }
-                                }
-
-                                @Override
-                                public void onCancelled(@NonNull DatabaseError error) {
-
                                 }
                             });
                         }
 
-                        @Override
-                        public void onCancelled(@NonNull DatabaseError error) {
+                        Toast.makeText(ProceedOrderActivity.this, "Order completed!", Toast.LENGTH_SHORT).show();
+                    }
 
-                        }
-                    });
-                }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
                 cartInfoList.clear();
                 Intent intent = new Intent(ProceedOrderActivity.this, HomeActivity.class);
                 setResult(RESULT_OK, intent);
                 finish();
-
             }
         });
 
@@ -235,7 +198,7 @@ public class ProceedOrderActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(ProceedOrderActivity.this, ChangeAddressActivity.class);
-                intent.putExtra("userId",userId);
+                intent.putExtra("userId", userId);
                 changeAddressLauncher.launch(intent);
                 //startActivity(new Intent(ProceedOrderActivity.this, ChangeAddressActivity.class));
             }
